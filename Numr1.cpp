782bce60eb492a0cd0cafbead60974b9bab62cb1
MateuszNowak1 <mateusz.nowak7265@gmail.com>
2020-06-23 16:26:53 +0200

    numer2

--- Numr1.cpp
+++ Numr1.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
